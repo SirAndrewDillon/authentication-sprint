fdba578c9ef10c46d07de42f0bb2fef053175924
andrew dillon <majik895@gmail.com>
2019-09-20 12:02:37 -0400

    :computer: having fun with messsages

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,70 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const Users = require('../jokes/jokes-model.js')
+const secrets = require('../config/secrets.js')
+const restricted = require('../auth/authenticate-middleware.js')
 
 router.post('/register', (req, res) => {
-  // implement registration
-});
+	// implement registration
+	let user = req.body
+	const hash = bcrypt.hashSync(user.password, 5)
+	user.password = hash
+
+	Users.addUser(user)
+		.then((user) => {
+			res.status(201).json(user)
+		})
+		.catch((error) => {
+			res.status(500) / json({ message: 'Say my name..USER.' })
+		})
+})
 
 router.post('/login', (req, res) => {
-  // implement login
-});
+	// implement login
+	let { username, password } = req.body
 
-module.exports = router;
+	Users.findUser({ username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = getJwt(user)
+				res.status(200).json({
+					message: `Welcome, you have the keys to immorality ${user.username}`,
+					token
+				})
+			} else {
+				res
+					.status(401)
+					.json({ message: 'You do not have the keys you will die!!' })
+			}
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Ouch that has to hurt!!' })
+		})
+})
+
+router.get('/users', restricted, (req, res) => {
+	Users.getUsers()
+		.then((users) => {
+			res.status(200).json(users)
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Come back later with a token!' })
+		})
+})
+
+function getJwt(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username,
+		jwtid: 1
+	}
+	const options = {
+		expiresIn: '1h'
+	}
+	return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
+module.exports = router
